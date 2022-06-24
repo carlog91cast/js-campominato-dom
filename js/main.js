475b56fe8fd41c01c9dd37976c9a6e95e15ce545
bigeppo <carlog91cast@gmail.com>
2022-06-24 12:03:55 +0200

    intro repo

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const playBtn = document.getElementById('btn-genera');
+const gridContainer = document.getElementById('mine-camp');
+
+// creo i songoli quadrati
+playBtn.addEventListener('click', function () {
+    gridContainer.innerHTML = "";
+    for (let i = 1; i <= 100; i++) {
+
+        // inserisco la funzione che crea i quadrati
+        inSquare = createSquares();
+        // inserisco la funzione nel button
+
+        // inserisco i quadrati nell'html con append
+        inSquare.innerHTML = i;
+        gridContainer.append(inSquare);
+
+
+    }
+
+});
+
+
+// in una fuzione creo i singoli quadrati
+
+function createSquares() {
+
+    const square = document.createElement('div')
+    square.classList.add('square-grid');
+    square.addEventListener('click', function () {
+        square.classList.toggle('active');
+    });
+
+
+    return square;
+
+};
